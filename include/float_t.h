fdac3c2e0c765dc8cf5423e41ffd4136770f79d4
kamil <kamil.klawikowski03@gmail.com>
2025-12-17 22:28:44 +0100

    feat: add standardized floating-point type aliases

---
+++ include/float_t.h
@@ -0,0 +1,15 @@
+/**
+ * @file float_t.h
+ * @brief Standardized floating-point type aliases
+ *
+ * @date 17-12-2025
+ */
+
+#ifndef MATHIX_FLOAT_T_H
+#define MATHIX_FLOAT_T_H
+
+typedef float float32_t;
+typedef double float64_t;
+typedef long double float128_t;
+
+#endif //MATHIX_FLOAT_T_H
