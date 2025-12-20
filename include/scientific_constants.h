ed1998d96abf8045c569c4dd8b5d8fe5710fbb87
kamil <kamil.klawikowski03@gmail.com>
2025-12-20 22:25:02 +0100

    feat: add scientific constants declarations and definitions

---
+++ include/scientific_constants.h
@@ -0,0 +1,79 @@
+/**
+ * @file scientific_constants.h
+ * @brief Scientific constants used by the calculator
+ *
+ * @date 20-12-2025
+ */
+
+#ifndef MATHIX_SCIENTIFIC_CONSTANTS_H
+#define MATHIX_SCIENTIFIC_CONSTANTS_H
+
+#include "float_t.h"
+#include "math.h"
+
+/*
+ * @brief PI constant
+ */
+extern const float64_t SC_PI;
+
+/*
+ * @brief Euler's constant
+ */
+extern const float64_t SC_EULER;
+
+/*
+ * @brief Boltzmann constant (J/K)
+ */
+extern const float64_t SC_BOLTZMANN_JK;
+
+/*
+ * @brief Boltzmann constant (eV/K)
+ */
+extern const float64_t SC_BOLTZMANN_eVK;
+
+/*
+ * @brief Boltzmann constant (Hz/K)
+ */
+extern const float64_t SC_BOLTZMANN_HzK;
+
+/*
+ * @brief Speed of light in vacuum (m/s)
+ */
+extern const float64_t SC_SPEED_OF_LIGHT;
+
+/*
+ * @brief Planck constant (J*s)
+ */
+extern const float64_t SC_PLANCK;
+
+/*
+ * @brief Gravity constant (m^3 * kg^-1 * s^-2)
+ */
+extern const float64_t SC_GRAVITATIONAL;
+
+/*
+ * @brief Avogadro constant (mol^-1)
+ */
+extern const float64_t SC_AVOGADRO;
+
+/*
+ * @brief Gas constant (J*mol^-1*K^-1)
+ */
+extern const float64_t SC_GAS;
+
+/*
+ * @brief Faraday constant (C/mol)
+ */
+extern const float64_t SC_FARADAY;
+
+/*
+ * @brief Elementary charge (C)
+ */
+extern const float64_t SC_ELEMENTARY_CHARGE;
+
+/*
+ * @brief Vacuum permittivity (F/m)
+ */
+extern const float64_t SC_EPSILON_0;
+
+#endif //MATHIX_SCIENTIFIC_CONSTANTS_H
---
+++ source/scientific_constants.c
@@ -0,0 +1,27 @@
+#include "../include/scientific_constants.h"
+
+const float64_t SC_PI = M_PI;
+
+const float64_t SC_EULER = M_E;
+
+const float64_t SC_BOLTZMANN_JK = 1.380649e-23;
+
+const float64_t SC_BOLTZMANN_eVK = 8.617333e-5;
+
+const float64_t SC_BOLTZMANN_HzK = 2.083661e10;
+
+const float64_t SC_SPEED_OF_LIGHT = 299792458;
+
+const float64_t SC_PLANCK = 6.62607015e-34;
+
+const float64_t SC_GRAVITATIONAL = 6.67430e-11;
+
+const float64_t SC_AVOGADRO = 6.02214076e23;
+
+const float64_t SC_GAS = 8.314462618;
+
+const float64_t SC_FARADAY = 96485.33212;
+
+const float64_t SC_ELEMENTARY_CHARGE = 1.602176634e-19;
+
+const float64_t SC_EPSILON_0 = 8.8541878128e-12;
